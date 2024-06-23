bee360b840bb285aa1b966f9f4b2aa0ad91fe59c
Enriquefft <enriquefft2001@gmail.com>
2024-06-22 23:14:36 -0500

    feat: added sample test for Button component

---
+++ tests/components/button.test.tsx
@@ -0,0 +1,13 @@
+import { expect, test } from "bun:test";
+import { Button } from "@/components/button";
+
+import { render, screen } from "@testing-library/react";
+
+test("uses jest-dom", () => {
+  render(<Button>Visible Example</Button>);
+
+    expect(screen.getByText("Visible Example")).toBeInTheDocument();
+
+    expect(screen.getByRole("button")).toHaveTextContent("Visible Example");
+
+});
