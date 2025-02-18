f882e790a8c66ed2d62996393cdaf3d407c663b5
Enriquefft <enriquefft2001@gmail.com>
2025-02-18 15:54:28 -0500

    feat: upgraded drizzle & other packages

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,6 @@
-import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
 import { env } from "@/env.ts";
 import * as schema from "./schema/post";
 
-const sql = neon(env.DRIZZLE_DATABASE_URL);
-export const db = drizzle(sql, { schema });
+export const db = drizzle(env.DRIZZLE_DATABASE_URL, { schema });
