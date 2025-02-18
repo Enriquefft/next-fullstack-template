d0f21bb2062525d2d96e4fd7467415652ce05b8d
Enriquefft <enriquefft2001@gmail.com>
2025-02-18 13:13:28 -0500

    style: format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-
 /**
  * @returns Home page component
  */
 export default async function Home() {
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
