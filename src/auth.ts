59964e2cc3976fa9cc87caae082a7309817a7dd8
Enriquefft <enriquefft2001@gmail.com>
2025-02-19 05:16:33 -0500

    style:format

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,12 @@
+import { db } from "@/db"; // your drizzle instance
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/db"; // your drizzle instance
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg",
-  }),
-  emailAndPassword: {
-    enabled: true,
-  },
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
 });
