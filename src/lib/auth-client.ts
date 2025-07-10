134412f9868120e5f5f53cccdf7da4538719ebd4
enrique flores <enriquefft2001@gmail.com>
2025-07-10 07:28:36 -0500

    feat: switch to better-auth polar integration

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,2 +1,7 @@
 import { createAuthClient } from "better-auth/react";
-export const { signIn, signUp, useSession } = createAuthClient({});
+import { polarClient } from "@polar-sh/better-auth";
+
+export const { signIn, signUp, useSession, checkout, customer } =
+	createAuthClient({
+		plugins: [polarClient()],
+	});
