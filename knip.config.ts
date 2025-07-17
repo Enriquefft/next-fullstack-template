4b94dbbb5c38263dd410a7be5bd5dc38a41c71a4
Enriquefft <enriquefft2001@gmail.com>
2025-07-17 16:42:48 -0500

    feat: added knip config

---
+++ knip.config.ts
@@ -0,0 +1,22 @@
+import type { KnipConfig } from "knip";
+
+const config: KnipConfig = {
+	compilers: {
+		css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/g)].join("\n"),
+	},
+	entry: [
+		"{tests,scripts}/**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}",
+		"src/metadata.ts",
+	],
+	// Files to exclude from Knip analysis
+	ignore: ["unlighthouse.config.ts", "src/components/ui/**/*"],
+	// Binaries to ignore during analysis
+	ignoreBinaries: [],
+	// Dependencies to ignore during analysis
+	ignoreDependencies: ["@hookform/devtools"],
+	paths: {
+		"@/*": ["./src/*"],
+	},
+};
+
+export default config;
